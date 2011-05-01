8a51c7f79388ceea71d74bf38082c9468b9a2cb0
axefan <bhansn@gmail.com>
2011-05-01 11:35:33 -0400

    Started adding redstone support.

--- src/us/axefan/plugin/RegionControl.java
+++ src/us/axefan/plugin/RegionControl.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 import javax.persistence.PersistenceException;
 
+import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sk89q.worldedit.bukkit.WorldEditAPI;
@@ -17,11 +19,13 @@ public class RegionControl extends JavaPlugin {
 	
 	private WorldEditPlugin worldEditPlugin = null;
 	private WorldEditAPI worldEditApi = null;
+    private final RegionControlBlockListener blockListener = new RegionControlBlockListener(this);
 	
 	public void onEnable() {
         PluginDescriptionFile desc = this.getDescription();
         System.out.println(desc.getName() + " starting...");
-//		PluginManager pm = getServer().getPluginManager();
+		PluginManager pm = getServer().getPluginManager();
+		//pm.registerEvent(Event.Type.REDSTONE_CHANGE, blockListener, Event.Priority.Normal, this);
 		this.getCommand("rc").setExecutor(new CommandExecutor (this));
 		this.worldEditPlugin = (WorldEditPlugin) this.getServer().getPluginManager().getPlugin("WorldEdit");
 		if (this.worldEditPlugin != null){
